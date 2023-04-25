1e60863d54b8f54f2ae61818c800759fe563c728
gruberfalls <70550492+mattfkaiser@users.noreply.github.com>
2023-04-25 14:49:52 -0400

    Create NRCLex_prelim.py

---
+++ NRCLex_prelim.py
@@ -0,0 +1,56 @@
+# Program that uses NRCLex to rate emotions associated with comments in comments.csv
+
+import pandas as pd
+import nltk
+from nrclex import NRCLex
+
+# Load the CSV file
+file_path = 'comments.csv'
+df = pd.read_csv(file_path)
+
+# Assuming the column containing comments is named 'Text'
+comments = df['Text'].astype(str)
+
+# Function to get the emotion ratings
+def get_emotion_ratings(text):
+    return NRCLex(text).affect_frequencies
+
+# Calculate emotion ratings for each comment
+emotions = []
+for comment in comments:
+    emotion = get_emotion_ratings(comment)
+    emotions.append(emotion)
+
+# Add emotion ratings to the DataFrame
+df['emotions'] = emotions
+
+# Save the DataFrame with emotion ratings to a new CSV file
+output_file_path = 'nrc_emotions.csv'
+df.to_csv(output_file_path, index=False)
+
+# Function to categorize emotion
+def categorize_emotion(emotions):
+    if emotions['positive'] > emotions['negative']:
+        return 'Positive'
+    elif emotions['positive'] < emotions['negative']:
+        return 'Negative'
+    else:
+        return 'Neutral'
+    
+# Categorize emotion for each comment
+df['emotion'] = df['emotions'].apply(categorize_emotion)
+
+# Save the DataFrame with emotion analysis to a new CSV file
+output_file_path = 'nrc_emotions_categorized.csv'
+df.to_csv(output_file_path, index=False)
+
+# Function to get the top emotion
+def get_top_emotion(emotions):
+    return max(emotions, key=emotions.get)
+
+# Get the top emotion for each comment
+df['top_emotion'] = df['emotions'].apply(get_top_emotion)
+
+# Save the DataFrame with emotion analysis to a new CSV file
+output_file_path = 'nrc_emotions_top.csv'
+df.to_csv(output_file_path, index=False)
