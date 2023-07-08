6dc5190154f4558bc22480e48da437ad063491c6
gruberfalls <70550492+mattfkaiser@users.noreply.github.com>
2023-07-08 15:26:43 -0400

    Update textBlob_Polarity.py

--- textBlob_Polarity.py
+++ textBlob_Polarity.py
@@ -2,7 +2,7 @@ import pandas as pd
 from textblob import TextBlob
 
 # Load the excel file
-file_path = 'ChatGPTComments.xlsx'
+file_path = 'TechnologyComments.xlsx'
 df = pd.read_excel(file_path)
 
 # Assuming the column containing comments is named 'Text'
@@ -26,9 +26,9 @@ df['sentiment_polarity'] = sentiments
 
 # Function to categorize sentiment
 def categorize_sentiment(polarity):
-    if polarity > 0.25:
+    if polarity > 0.1:
         return 'Positive'
-    elif polarity < -0.25:
+    elif polarity < -0.1:
         return 'Negative'
     else:
         return 'Neutral'
@@ -38,5 +38,5 @@ def categorize_sentiment(polarity):
 df['sentiment'] = df['sentiment_polarity'].apply(categorize_sentiment)
 
 # Save the DataFrame with sentiment analysis to a new CSV file
-output_file_path = 'textBlob_polarity_chatgpt.csv'
+output_file_path = 'textBlob_polarity_technology.csv'
 df.to_csv(output_file_path, index=False)
